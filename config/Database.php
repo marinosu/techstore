b32d38e86e39f1b700ccc2b8b5bbbe473e6d2bed
marinosu <msantos0138@gmail.com>
2026-09-19 02:26:56 -0500

    agregando el puerto a database.php

--- config/Database.php
+++ config/Database.php
@@ -8,6 +8,7 @@ class Database
     private string $username;
     private string $password;
     private string $database;
+    private int $port;
 
     private ?mysqli $connection = null;
 
@@ -29,6 +30,7 @@ class Database
         $this->username = $_ENV['DB_USERNAME'] ?? getenv('DB_USERNAME') ?: 'root';
         $this->password = $_ENV['DB_PASSWORD'] ?? getenv('DB_PASSWORD') ?: '';
         $this->database = $_ENV['DB_DATABASE'] ?? getenv('DB_DATABASE') ?: 'techstore';
+        $this->port = (int) ($_ENV['DB_PORT'] ?? getenv('DB_PORT') ?: 3306);
     }
 
     public function connect(): mysqli
@@ -38,7 +40,8 @@ class Database
                 $this->host,
                 $this->username,
                 $this->password,
-                $this->database
+                $this->database,
+                $this->port
             );
 
             if ($this->connection->connect_error) {
